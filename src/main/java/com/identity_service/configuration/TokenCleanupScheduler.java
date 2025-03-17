6b9a50b274e91bbf3e0acd310a2b7b41ac040b63
nt-myduyen <myduyen14125@gmail.com>
2025-03-17 22:57:05 +0700

    schedule delete invalidated token

---
+++ src/main/java/com/identity_service/configuration/TokenCleanupScheduler.java
@@ -0,0 +1,28 @@
+package com.identity_service.configuration;
+
+import com.identity_service.repository.InvalidatedTokenRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TokenCleanupScheduler {
+    InvalidatedTokenRepository tokenRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void scheduleCollectInvalidatedToken() {
+        log.info("Running token cleanup at {}", LocalDateTime.now());
+        int deletedCount = tokenRepository.findAll().size();
+        tokenRepository.deleteAll();
+        log.info("Deleted " + deletedCount + " tokens");
+    }
+}
